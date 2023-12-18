c1999c017196429a6eda150a19b5fec34a4f71cb
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2023-12-18 22:45:53 +0530

    IF..ELSE.. Statment Implementation

--- shell.py
+++ shell.py
@@ -1,8 +1,10 @@
 import basic
 
 while True:
-    text = input('basic > ')
-    result, error = basic.run('<stdin>', text)
+    text = input("basic > ")
+    result, error = basic.run("<stdin>", text)
 
-    if error: print(error.as_string())
-    else: print(result)
+    if error:
+        print(error.as_string())
+    elif result:
+        print(result)
